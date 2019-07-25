151569a1a2d000b1cc65b3d4ae37605e8e730b89
Justine Gennaro <justine.rouzier@gmail.com>
2019-07-25 15:54:53 -0500

    step 1 done

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -4,5 +4,15 @@ const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item? JSON.parse(item) : initialValue;
+        
+        function setValue(value) {
+            setStoredValue(value)
+            window.localStorage.setItem(key, JSON.stringify(value))
+        }
     })
+
+    return [storedValue]
 }
+
+
+export default useLocalStorage;
