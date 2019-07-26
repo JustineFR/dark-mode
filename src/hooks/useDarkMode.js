64622152e00a72c917d5bdf90326caac2fb70070
Justine Gennaro <justine.rouzier@gmail.com>
2019-07-25 19:41:21 -0500

    mvp

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,20 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import useLocalStorage from "./useLocalStorage";
 
 
 const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage('key')
+    const [darkMode, setDarkMode] = useLocalStorage("dark-mode")
+
+    useEffect(() => {
+        if (darkMode === true) {
+            document.body.classList.add('dark-mode')
+        } else {
+            document.body.classList.remove('dark-mode')
+        }
+    
+        
+    
+    },[darkMode]);
+    
+    return [darkMode, setDarkMode]
 }
 
-useEffect(() => {
-    if (darkMode === true) {
-        document.body.classList.add('dark-mode')
-    } else {
-        document.body.classList.remove('dark-mode')
-    }
-
-    return (darkMode, setDarkMode)
-    
-},[darkMode])
-
 export default useDarkMode;
+
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,14 +5,16 @@ const useLocalStorage = (key, initialValue) => {
         const item = window.localStorage.getItem(key);
         return item? JSON.parse(item) : initialValue;
         
-        function setValue(value) {
-            setStoredValue(value)
-            window.localStorage.setItem(key, JSON.stringify(value))
-        }
+        
     })
 
-    return [storedValue, setValue]
-}
+    const setValue = value => {
+        setStoredValue(value)
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
+
+    return [storedValue, setValue];
+};
 
 
 export default useLocalStorage;
